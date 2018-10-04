604e2bca588c7bd3c19a0b963feabbfe1111abfd
Zachary M. Mattis <zmm15@pitt.edu>
2018-10-04 14:59:56 -0400

    Added lab2.1

---
+++ ECE-1160/Raspberry-Pi-IO/l2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+    
+# course:     ECE 1160
+# laboratory: 2
+# date:       10/04/18
+# username:   zmm15
+# name:       Zachary M. Mattis
+# description: a basic start-up program.
+
+
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+sense.show_message("Hello world!")
