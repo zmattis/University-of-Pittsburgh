f03610469d7d063e133fb112644ac786d98be1da
Zachary M. Mattis <zachary.mattis@pitt.edu>
2018-10-04 17:09:34 -0400

    Update l2

--- ECE-1160/Raspberry-Pi-IO/l2.py
+++ ECE-1160/Raspberry-Pi-IO/l2.py
@@ -10,6 +10,28 @@
 
 from sense_hat import SenseHat
 
-sense = SenseHat()
+RED = []
 
-sense.show_message("Hello world!")
+
+class Color(object):
+    BLACK   = [000, 000, 000];
+    RED     = [255, 000, 000];
+    GREEN   = [000, 255, 000];
+    BLUE    = [000, 000, 255];
+    YELLOW  = [255, 255, 000];
+    CYAN    = [000, 255, 255];
+    MAGENTA = [255, 000, 255];
+    WHITE   = [255, 255, 255];
+
+
+class Smiley():
+    
+
+def __init__(self):
+    self.
+
+
+
+
+smiley = Smiley();
+smiley.scroll();
